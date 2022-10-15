6c3282bfdf6c48a9cdc1761581351440730c0772
David-0717 <ot18005@ues.edu.sv>
2022-10-15 17:12:08 -0600

    Se agregaron componentes

--- sirt-client/src/app/app-routing.module.ts
+++ sirt-client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlanAlimenticioComponent } from './components/plan-alimenticio/plan-alimenticio.component';
 
 const routes: Routes = [
-
+{
+  path:'',
+  component:PlanAlimenticioComponent
+}
 ];
 
 @NgModule({
--- sirt-client/src/app/app.module.ts
+++ sirt-client/src/app/app.module.ts
@@ -4,14 +4,19 @@ import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatInputModule } from '@angular/material/input'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlanAlimenticioComponent } from './components/plan-alimenticio/plan-alimenticio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PlanAlimenticioComponent,
   ],
   imports: [
     BrowserModule,
@@ -20,7 +25,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule,
+    CdkTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
